dc85a1388f4a6f0031d4102459bb7c873b5cbb85
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-03-31 15:32:53 +0600

    display countries in a simple way

--- src/App.js
+++ src/App.js
@@ -26,6 +26,18 @@ function LoadCountries() {
     <div>
       <h1>Visiting Every Country of the World!!!</h1>
       <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population} ></Country>)
+      }
+    </div >
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population} </h4>
     </div>
   )
 }
